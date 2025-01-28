0936bf71e2dabf4bfc1a22409443e2bec51aa5a8
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-28 10:47:14 +0000

    definicion del constructor de la clase fabrica

--- concesionario/con.cpp
+++ concesionario/con.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 class fabrica{
     public:
-        
+
+        fabrica(r,c,m,mr,p){
+            ruedas = r;
+            color = c;
+            modelo = m;
+            marca = mr;
+            proposito = p;
+        }
+    private:
+    int ruedas;
+    string color;
+    string modelo;
+    string marca;
+    string proposito;
 }
